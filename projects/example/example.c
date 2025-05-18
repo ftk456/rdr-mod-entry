e74c9884944a155a720c123121f50c127ecd42b0
ftk456 <58621117+ftk456@users.noreply.github.com>
2025-05-18 15:47:33 +0200

    Update example.c

--- projects/example/example.c
+++ projects/example/example.c
@@ -8,31 +8,31 @@ void main_loop()
 {
 	while (true)
 	{
-		//your code
+		//ton code ici
 
 		WAIT(0);
 	}
 }
 
-void start_script(int scriptID, char* scriptName)
+void start_script(int scriptID, char* scriptName) //démarer le script pour pouvoir l'utiliser dans le jeu
 {
 	if (IS_SCRIPT_VALID(scriptID))
 	{
-		STREAMING_REQUEST_SCRIPT(scriptID);
+		STREAMING_REQUEST_SCRIPT(scriptID); //on demande d'éxecuter le script
 
 		while (!STREAMING_IS_SCRIPT_LOADED(scriptID))
 		{
 			WAIT(0);
 		}
 
-		if (STREAMING_IS_SCRIPT_LOADED(scriptID))
+		if (STREAMING_IS_SCRIPT_LOADED(scriptID)) //quand il est chargé
 		{
 			if (scriptName != "")
 			{
 				START_NEW_SCRIPT(scriptName, 0);
 			}
 			else {
-				START_NEW_SCRIPT(GET_SCRIPT_NAME(), 0);
+				START_NEW_SCRIPT(GET_SCRIPT_NAME(), 0); //je ne connais pas vraiment le 2ème paramètre
 			}
 		}
 	}
@@ -45,11 +45,11 @@ void main()
 	{
 		if (!IS_GAME_PAUSED())
 		{
-			if (DOES_SCRIPT_EXIST("$/content/release/scripting/designerdefined/ui/fuieventmonitor.csc"))
+			if (DOES_SCRIPT_EXIST("$/content/release/scripting/designerdefined/ui/fuieventmonitor.csc")) //script d'affichage
 			{
 				LAUNCH_NEW_SCRIPT("$/content/release/scripting/designerdefined/ui/fuieventmonitor.csc", 0);
 				start_script(scriptID, "");
-				main_loop();
+				main_loop(); //on passe à notre boucle principale
 			}
 		}
 	}
